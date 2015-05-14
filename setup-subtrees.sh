e24e1dedbae13a493455054c6a176a210688a191
cmotc <cmotc@openmailbox.org>
2015-05-14 04:02:46 -0400

    adjusted packages, submodules

---
+++ setup-subtrees.sh
@@ -0,0 +1,8 @@
+git subtree -P pkgs git@github.com:AvANa-BBS/freepto-certificates.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/freepto-docs.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/freepto-graphics.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/freepto-passwords-changer.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/freepto-usb-utils.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/synclient-tools.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/Tomb.git master
+git subtree -P pkgs git@github.com:AvANa-BBS/stem-tortp.git master
