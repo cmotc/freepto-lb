5c6fd7b35ac5115218540105ffde35003196b39f
cmotc <cmotc@openmailbox.org>
2015-05-14 04:37:51 -0400

    apt-transport-https trick(see config/freepto) so I can add i2p

--- setup-subtrees.sh
+++ setup-subtrees.sh
@@ -1,8 +1,18 @@
-git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-certificates.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-docs.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-graphics.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-passwords-changer.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-usb-utils.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/synclient-tools.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/Tomb.git master
-git subtree pull -P pkgs git@github.com:AvANa-BBS/stem-tortp.git master
+
+git clone git@github.com:AvANa-BBS/freepto-certificates.git pkgs/certificates
+git clone git@github.com:AvANa-BBS/freepto-docs.git pkgs/freepto-docs
+git clone git@github.com:AvANa-BBS/freepto-graphics.git pkgs/freepto-graphics
+git clone git@github.com:AvANa-BBS/freepto-passwords-changer.git pkgs/freepto-passwords-changer
+git clone git@github.com:AvANa-BBS/freepto-usb-utils.git pkgs/freepto-usb-utils
+git clone git@github.com:AvANa-BBS/synclient-tools.git pkgs/synclient-tools
+git clone git@github.com:AvANa-BBS/Tomb.git pkgs/tomb
+git clone git@github.com:AvANa-BBS/stem-tortp.git pkgs/tortp_stem
+
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-certificates.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-docs.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-graphics.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-passwords-changer.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-usb-utils.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/synclient-tools.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/Tomb.git master
+#git subtree pull -P pkgs git@github.com:AvANa-BBS/stem-tortp.git master
