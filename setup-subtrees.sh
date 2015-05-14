bd93a5e69a7e01ae71e4bf1ca7aaa9d3a2004084
cmotc <cmotc@openmailbox.org>
2015-05-14 04:40:07 -0400

    apt-transport-https trick(see config/freepto) so I can add i2p

--- setup-subtrees.sh
+++ setup-subtrees.sh
@@ -1,12 +1,21 @@
 
-git clone git@github.com:AvANa-BBS/freepto-certificates.git pkgs/certificates
-git clone git@github.com:AvANa-BBS/freepto-docs.git pkgs/freepto-docs
-git clone git@github.com:AvANa-BBS/freepto-graphics.git pkgs/freepto-graphics
-git clone git@github.com:AvANa-BBS/freepto-passwords-changer.git pkgs/freepto-passwords-changer
-git clone git@github.com:AvANa-BBS/freepto-usb-utils.git pkgs/freepto-usb-utils
-git clone git@github.com:AvANa-BBS/synclient-tools.git pkgs/synclient-tools
-git clone git@github.com:AvANa-BBS/Tomb.git pkgs/tomb
-git clone git@github.com:AvANa-BBS/stem-tortp.git pkgs/tortp_stem
+git clone https://github.com/AvANa-BBS/freepto-certificates.git pkgs/certificates
+git clone https://github.com/AvANa-BBS/freepto-docs.git pkgs/freepto-docs
+git clone https://github.com/AvANa-BBS/freepto-graphics.git pkgs/freepto-graphics
+git clone https://github.com/AvANa-BBS/freepto-passwords-changer.git pkgs/freepto-passwords-changer
+git clone https://github.com/AvANa-BBS/freepto-usb-utils.git pkgs/freepto-usb-utils
+git clone https://github.com/AvANa-BBS/synclient-tools.git pkgs/synclient-tools
+git clone https://github.com/AvANa-BBS/Tomb.git pkgs/tomb
+git clone https://github.com/AvANa-BBS/stem-tortp.git pkgs/tortp_stem
+
+#git clone git@github.com:AvANa-BBS/freepto-certificates.git pkgs/certificates
+#git clone git@github.com:AvANa-BBS/freepto-docs.git pkgs/freepto-docs
+#git clone git@github.com:AvANa-BBS/freepto-graphics.git pkgs/freepto-graphics
+#git clone git@github.com:AvANa-BBS/freepto-passwords-changer.git pkgs/freepto-passwords-changer
+#git clone git@github.com:AvANa-BBS/freepto-usb-utils.git pkgs/freepto-usb-utils
+#git clone git@github.com:AvANa-BBS/synclient-tools.git pkgs/synclient-tools
+#git clone git@github.com:AvANa-BBS/Tomb.git pkgs/tomb
+#git clone git@github.com:AvANa-BBS/stem-tortp.git pkgs/tortp_stem
 
 #git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-certificates.git master
 #git subtree pull -P pkgs git@github.com:AvANa-BBS/freepto-docs.git master
